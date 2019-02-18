ba642d46d05a9fabb93aa4a1791b4005c847a25f
ZEUS\m4rt00 <m4rt00@gmail.com>
2019-02-18 08:02:27 +0200

    add custom Serliog

---
+++ src/Common.Service/Serilog/SerilogExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Serilog;
+
+namespace Common.Service.Serilog
+{
+    public static class SerilogExtensions
+    {
+        public static IWebHostBuilder UseCustomSerilog(this IWebHostBuilder builder)
+        {
+            return builder
+                .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+                    .ReadFrom.Configuration(hostingContext.Configuration));
+        }
+    }
+}
